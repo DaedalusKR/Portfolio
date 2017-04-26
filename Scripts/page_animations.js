e266c01087a326e2419b9e295b49e39d2ad66530
Ken Roberts <kr26633z@gmail.com>
2017-04-26 23:31:58 +0100

    Animation Update
    Transitioning animations fro CSS to JQuery

---
+++ Scripts/page_animations.js
@@ -0,0 +1,8 @@
+function home_anims() {
+  $(document).ready(function(){
+    $('#nav_bar').animate({width: '100%'});
+    $('#portfolio_main_space').animate({width: '100%'});
+    $('#main_white_cover').animate({width: '0%'});
+    $('#home_footer').animate({width: '100%'});
+  });
+}
