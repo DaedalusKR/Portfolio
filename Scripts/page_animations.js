c52dae281648ade17ed00202280ad3628b4cf98e
Ken Roberts <kr26633z@gmail.com>
2017-04-27 12:46:10 +0100

    New JQuery Scripts for Page Anims
    New scripts

--- Scripts/page_animations.js
+++ Scripts/page_animations.js
@@ -1,8 +1,32 @@
 function home_anims() {
-  $(document).ready(function(){
-    $('#nav_bar').animate({width: '100%'});
-    $('#portfolio_main_space').animate({width: '100%'});
-    $('#main_white_cover').animate({width: '0%'});
-    $('#home_footer').animate({width: '100%'});
-  });
+  
+	$(document).ready(function(){
+    
+		$('#nav_bar').animate({width: '100%'});
+    
+		$('#portfolio_main_space').animate({width: '100%'});
+    				
+		$('#main_white_cover').animate({width: '0%'});
+    
+		$('#home_footer').animate({width: '100%'});
+  
+	});
+
+}
+
+
+function project_anims() {
+	$(document).ready(function() {
+		$('#portfolio_main_space_projects').animate({width: '20%'});
+	});
+
+
+}
+
+function contact_anims() {
+	$(document).ready(function(){
+		
+
+	});
+
 }
