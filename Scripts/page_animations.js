f0479cfbf97c9ee257444c3eaa016a3afdafd667
Ken Roberts <kr26633z@gmail.com>
2017-05-04 18:41:34 +0100

    animations based on previous page added

--- Scripts/page_animations.js
+++ Scripts/page_animations.js
@@ -2,17 +2,17 @@ function home_anims() {
 	$(document).ready(function(){
 
 		function home_anim_p1() {
-			$('#nav_bar').animate({width: '100%'}, 500);
-			$('#portfolio_main_space').animate({width: '100%'}, 500);
-			$('#main_white_cover').animate({width: '0%'}, 500);
-			$('#home_footer').animate({width: '100%'}, 500, home_anim_p2());
+				$('#nav_bar').animate({width: '100%'}, 500);
+				$('#portfolio_main_space').animate({width: '100%'}, 500);
+				$('#main_white_cover').animate({width: '0%'}, 500);
+				$('#home_footer').animate({width: '100%'}, 500, home_anim_p2());
 		}
 
 		function home_anim_p2() {
 			$('#main_white_cover').height('0px');
 			$('#light_green_cover').delay(500).animate({height: '275px'});
 			$('#portfolio_main_space').delay(0).animate({height: '325px'});
-			$('#nav_list, #nav_logo, #footer_text').hide().fadeIn(500).animate({opacity: 1});
+			$('#nav_list, #nav_logo, #footer_text').hide().fadeIn(500).animate({opacity: 1}, setCookie('page', 'index.html'));
 		}
 		home_anim_p1();
 	});
@@ -27,16 +27,22 @@ function project_anims() {
 
 		function projects_anim_p2() {
 			$('#portfolio_main_space_projects_light').animate({height: '0px'}, 250);
-			$('#portfolio_main_space_projects_dark').animate({height: '600px'}, 250, setCookie('page', 'project.html', 100));
+			$('#portfolio_main_space_projects_dark').animate({height: '600px'}, 250, setCookie('page', 'project.html'));
 			$('#nav_list, #nav_logo, #footer_text').css({opacity: 1});
 		}
 		projects_anim_p1();
-		
 	});
 }
 
 function contact_anims() {
 	$(document).ready(function() {
+
+		var last_visited = checkCookie();
+
+		if (last_visited == 'project.html') {
+			$('#portfolio_main_space_contact').css('width', '20%');
+			$('#portfolio_main_space_contact').animate({width: '100%'});
+		}
 
 		function contact_bg_anim_ph1() {
 			$('#main_white_cover').height('0px');
@@ -54,21 +60,16 @@ function contact_anims() {
 			$('.contact_label').fadeIn(100);
 			$('.form_input_field').animate({opacity: 1});
 			$('.form_input_field').fadeIn(250);
-			$('.form_submit').fadeIn(500);
+			$('.form_submit').fadeIn(500), setCookie('page', 'contact.html');
 		}
 		contact_bg_anim_ph1();
 	});
 }
 
 
-function setCookie(cname, cvalue, expdays) {
-	var d = new Date();
-	d.setTime(d.getTime() + (expdays*24*60*60*1000));
-	var expires = "expires=" + d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cname, cvalue) {
+	document.cookie = cname + "=" + cvalue + ";path=/";
 }
-
-
 
 function getCookie(cname) {
 	var name = cname + "=";
@@ -83,13 +84,9 @@ function getCookie(cname) {
 		}
 	}
 	return "";
-
 }
-
 
 function checkCookie() {
 	var last_visited = getCookie("page");
-		
+	return last_visited;
 }
-
-
