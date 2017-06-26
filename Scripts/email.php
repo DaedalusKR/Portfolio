8325356ea7ef565e0a264da7c54b82fc2e1ea1d7
Ken Roberts <kr26633z@gmail.com>
2017-06-26 19:04:43 +0100

    php test

--- Scripts/email.php
+++ Scripts/email.php
@@ -2,7 +2,7 @@
 echo "testing";
   $to = "ken@kenroberts.xyz";
   $subject = "My subject";
-  $txt = "Hello world!";
+  $txt = $_POST['message_body_text']
   $headers = "From: kr26633z@gmail.com";
 
   mail($to,$subject,$txt,$headers);
