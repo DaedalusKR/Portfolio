3263140c5a3c07c4202873551eaf044e2082deaa
Ken Roberts <kr26633z@gmail.com>
2017-07-18 22:41:10 +0100

    refactored remaining JS files

--- Scripts/page_actions.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('.form_submit').on('click', function() {
-    //PHP code for sending email to go here
-  });
-})
--- Scripts/spinner.js
+++ Scripts/spinner.js
@@ -1,9 +1,13 @@
-
+  //set base values for offsets and image properties
 var current_image_index = 0;
 var current_offset_value = 0;
 var image_width = 300;
 var position = 0;
 
+  //right_ and left_ work similarly. Find the current position on the image in the
+  //frame, this works by knowing the value of the offset divided by the image width.
+  //Move the image position left or right using translate3d to slide. If hit end
+  //of the container then flick back round.
 function right_clicked() {
   if (current_image_index <= 1) { //need to understand why its 1 for 3 images
     current_image_index++;
